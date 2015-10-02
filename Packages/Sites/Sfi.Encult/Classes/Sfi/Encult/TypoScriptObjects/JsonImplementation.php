b58db29b071ea653545ecb497c41d8b23f6129a7
Dmitri Pisarev <dimaip@gmail.com>
2015-10-02 15:34:32 +0300

    [FEATURE] TS object for rendering TS array into Json
    Converts TS key-value pairs and renders them into Json object.
    Amazingly hackish approach that could be made much cleaner, but it works
    for now.
    
    Coule be used to render a set of nodes into Json array like this:
    
    ```
    prototype(Sfi.Encult:JsonArrayOfSomeAnswers) {
    	content = TYPO3.TypoScript:Collection {
    		collection = ${q(node).children('[instanceof
    Sfi.Encult:Answer]').get()}
    		itemRenderer = Sfi.Encult:AnswerJson
    		itemName = 'node'
    		iterationName = 'iterator'
    	}
    	content.@process.1 = ${'[' + value + ']'}
    }
    
    prototype(Sfi.Encult:AnswerJson) < prototype(Sfi.Encult:Json) {
    	id = ${q(node).property('_name')}
    	authorName = ${q(node).property('authorName')}
    	worldviewId = ${q(node).property('worldview').name}
    	@process.1 = ${iterator.isLast ? value : value + ','}
    }
    ```

---
+++ Packages/Sites/Sfi.Encult/Classes/Sfi/Encult/TypoScriptObjects/JsonImplementation.php
@@ -0,0 +1,31 @@
+<?php
+namespace Sfi\Encult\TypoScriptObjects;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TypoScript\TypoScriptObjects\ArrayImplementation;
+
+/**
+ * Evaluate sub objects to json
+ */
+class JsonImplementation extends ArrayImplementation {
+
+	/**
+	 * @return string
+	 */
+	public function evaluate() {
+		$sortedChildTypoScriptKeys = $this->sortNestedTypoScriptKeys();
+
+		if (count($sortedChildTypoScriptKeys) === 0) {
+			return '';
+		}
+
+		$output = '';
+		foreach ($sortedChildTypoScriptKeys as $key) {
+			$value = $this->tsValue($key);
+			$output[$key] = $value;
+		}
+
+		return json_encode($output, JSON_UNESCAPED_UNICODE);
+	}
+
+}
